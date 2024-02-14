0cbb7ebb0761a7afad0314c749b6f08ab3f6d2e1
nestorss05 <nestor.sanchez@iesnervion.es>
2024-02-14 19:33:11 +0100

    Ejercicio 8 FINAL

--- src/ejercicio08/ClaseMain.java
+++ src/ejercicio08/ClaseMain.java
@@ -18,15 +18,23 @@ public class ClaseMain {
 		// String frase: frase a analizar
 		String frase;
 
+		// boolean esPalindroma: booleana que te mostrara si una frase es palindroma
+		boolean esPalindroma;
+
 		// Inicia el Scanner
 		Scanner sc = new Scanner(System.in);
 
 		// Pide al usuario una frase
 		System.out.println("Inserta una frase");
-		frase = sc.nextLine();
+		frase = sc.nextLine().toLowerCase();
 
-		// Llama a la funcion Analizar.frase y guarda su resultado en nApariciones
-
+		// Llama a la funcion IdefClase.esPalindroma y guarda su resultado en
+		// nApariciones
+		esPalindroma = IdefClase.esPalindroma(frase);
+		
+		// Muestra el resultado por pantalla
+		System.out.println(esPalindroma);
+		
 		// Cierra el Scanner
 		sc.close();
 
